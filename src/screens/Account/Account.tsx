54fc25252c8a4f9bf90661e931dfc5c600e017f2
Awank <mas.awank100@gmail.com>
2021-08-15 11:39:39 +0700

    implement redux on AccountScreen

--- src/screens/Account/Account.tsx
+++ src/screens/Account/Account.tsx
@@ -5,18 +5,36 @@ import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
 import {RootStackParamList} from '@components';
 import {HomeBottomTabParamList} from 'src/components/Navigation/HomeBottomTabNavigation';
+import {useAppSelector} from '@redux';
 
 export type AccountScreenNavigationProps = NativeStackNavigationProp<
-  HomeBottomTabParamList,
-  'Account'
->;
+  RootStackParamList,
+  'HomeBottomTab'
+> &
+  NativeStackNavigationProp<HomeBottomTabParamList, 'Account'>;
 
 export const AccountScreen = () => {
   const navigation = useNavigation<AccountScreenNavigationProps>();
+  const {token} = useAppSelector(state => state.account);
 
   return (
     <View>
       <Text style={{fontSize: 20}}>Account screen</Text>
+
+      {!token ? (
+        <>
+          <Text style={{fontSize: 16, marginTop: 20}}>
+            You're not logged in
+          </Text>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('Login')}
+            style={{padding: 16, backgroundColor: 'pink', marginTop: 40}}>
+            <Text>Goto Login</Text>
+          </TouchableOpacity>
+        </>
+      ) : (
+        <Text style={{fontSize: 16, marginTop: 20}}>Hi John</Text>
+      )}
     </View>
   );
 };
